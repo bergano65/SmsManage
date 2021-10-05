f8ba7957fd26d5921eb2b8e765dbe4d7aaffbdda
bergano65 <eviten@Microsoft.com>
2021-10-05 18:44:55 +0300

    - send mail added

--- SendAndReceiveSMS/SendAndReceiveSMS/MainPage.xaml.cs
+++ SendAndReceiveSMS/SendAndReceiveSMS/MainPage.xaml.cs
@@ -3,10 +3,19 @@ using SendAndReceiveSMS.Interfaces;
 using System;
 using Xamarin.Forms;
 
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading;
+using System.ComponentModel;
+
 namespace SendAndReceiveSMS
 {
     public partial class MainPage : ContentPage
     {
+        private string userName = "---";
+        private string password = "---";
+
         public MainPage()
         {
             InitializeComponent();
@@ -17,6 +26,24 @@ namespace SendAndReceiveSMS
         private void GlobalEvents_OnSMSReceived(object sender, SMSEventArgs e)
         {
             EntryMessage.Text = e.Message;
+            SendMail(e.Message);
+        }
+
+        private void SendMail(string message)
+        {            System.Net.NetworkCredential nc = new
+            NetworkCredential(userName, password);
+            MailMessage MailMessage = new MailMessage();
+            MailMessage.To.Add(userName);
+            MailMessage.Subject = "sms redirect";
+            MailMessage.From = new System.Net.Mail.MailAddress(userName);
+            MailMessage.Body = message;
+            System.Net.Mail.SmtpClient SmtpClient = new System.Net.Mail.SmtpClient("smtp-mail.outlook.com");
+            SmtpClient.UseDefaultCredentials = false;
+
+            SmtpClient.EnableSsl = true;
+            SmtpClient.Credentials = nc;
+            SmtpClient.Port = 587;
+            SmtpClient.Send(MailMessage);
         }
 
         /// <summary>
